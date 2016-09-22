6dfef01f6551a0c2e8776fb9f735c818d082f1ef
Juan Liska <jpliska@gmail.com>
2016-09-22 11:25:46 -0600

    add Version.java to git so that folder will exist

---
+++ src/generated/java/com/guatec/internal/Version.java
@@ -0,0 +1,11 @@
+package com.guatec.internal;
+
+public class Version {
+    public static String getShortVersion(){
+        return "1.1.2";
+    }
+
+    public static String getVersion(){
+        return "1.1.2" + "-" + "80f7cefe6eba81fb812401809b8fd1dc4b8760f9";
+    }
+}
