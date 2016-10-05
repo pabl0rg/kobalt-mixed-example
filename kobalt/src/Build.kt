3ecd76103e76b062ebb0233eb9f88793ff9d40b1
Juan Liska <jpliska@gmail.com>
2016-10-05 13:44:59 -0600

    add a bunch of dependencies
    one of these causes a huge fat jar to be generated (with a lot of appengine stuff)

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -47,8 +47,35 @@ val p = project {
         compile("com.h2database:h2:jar:1.4.192")
         //compile("org.flywaydb:flyway-core:4.0")
         compile(file("lib/flyway-core-0-SNAPSHOT.jar"))
+        compile("org.apache.activemq:activemq-client:5.11.3")
+        compile("org.apache.activemq:activemq-pool:5.11.3")
+        compile("commons-validator:commons-validator:1.4.0")
+        compile("org.apache.commons:commons-email:1.3.2")
+        compile("org.twitter4j:twitter4j-core:3.0.4")
+        compile("org.twitter4j:twitter4j-async:3.0.4")
+        compile("com.fasterxml.jackson.core:jackson-databind:2.7.1")
+        compile("com.fasterxml.jackson.core:jackson-core:2.7.1")
+        compile("com.fasterxml.jackson.core:jackson-annotations:2.7.1")
+        compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.7.1-2")
+        compile("org.codehaus.woodstox:woodstox-core-asl:4.4.1")
+        compile("joda-time:joda-time:2.2")
+        compile("com.squareup.okhttp3:okhttp:3.2.0")
+        compile("com.squareup.okio:okio:1.6.0")
+        compile("com.squareup.retrofit:converter-jackson:1.9.0")
+        compile("com.beust:jcommander:1.48")
+        compile("javax.jms:jms:1.1")
+        compile("javax.mail:mail:1.4")
+        compile("org.postgresql:postgresql:9.2-1004-jdbc41")
+        compile("org.zeromq:jeromq:0.3.5")    //inter-process comm
+        compile("commons-discovery:commons-discovery:0.4")
+        compile("wsdl4j:wsdl4j:1.6.2")
+        compile("org.fusesource.hawtbuf:hawtbuf:1.11")
+        compile("io.github.lukehutch:fast-classpath-scanner:1.9.18")
+        compile("org.fusesource.hawtbuf:hawtbuf:1.11")
+        compile("com.timgroup:java-statsd-client:3.1.0")
+        compile("org.flywaydb:flyway-core:4.0")
     }
-
+    
     dependenciesTest {
         compile("org.testng:testng:6.9.5")
     }
